c6716df5586939b61139257a4b01bb691e9986cf
niki4Him <nicson@mail.bg>
2017-05-05 15:26:56 +0300

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($)
+{	
+$(".tfdate").datepicker({
+    dateFormat: 'D, M d, yy',
+    showOn: 'button',
+    buttonImageOnly: true,
+    numberOfMonths: 3
+
+    });
+});
